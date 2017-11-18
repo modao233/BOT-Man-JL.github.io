84623c1ab1d304cda43634aaa6ba63d48d77f971
BOT-Man-JL <g123nn@hotmail.com>
2017-11-18 19:14:41 +0800

    Update CRLF

--- articles/2017/Macro-Pollution/main.cpp
+++ articles/2017/Macro-Pollution/main.cpp
@@ -7,8 +7,7 @@
 #undef min
 #endif
 
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
     auto validArgumentCount = std::min (argc, 2);
     auto workingDirectory = PathHelper::PathAppend (argv[0], "..\\");
     return 0;
